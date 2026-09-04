d5d74df500407582aa401264d3b5d85b18fceafd
Joseph <Josephson080@gmail.com>
2026-09-04 15:06:13 +0800

    my fourth post

--- Python_project/Python/INF1103_LAB_Folder/hello.py
+++ Python_project/Python/INF1103_LAB_Folder/hello.py
@@ -1,21 +1,36 @@
-print("------------------")
-print("Welcome here ")
-print("my first post")
-print("------------------")
+#Activity 1: 
+#print("------------------")
+#print("Welcome here ")
+#print("my first post")
+#print("------------------")
 
-username = "john_doe"
-bio = "I am a software developer"
-followers = 100
+#Activity 2:
+#username = "john_doe"
+#bio = "I am a software developer"
+#followers = 100
 
+
+
+#print("Username: " + username)
+#print("Bio: " + bio)
+#print("Followers: " + str(followers))
+
+#Activity 3:
+#followers += 50
+#print("Followers: " + str(followers))
+
+##followers += 20
+#print("Followers: " + str(followers))
+
+#followers += 10
+#print("Followers: " + str(followers))
+
+username = input("Enter your username: ")
+age = (input("Enter your age: "))
+category = input("Enter your category: ")
+
+print("\n Instagram Profile: ")
+print("=====================")
 print("Username: " + username)
-print("Bio: " + bio)
-print("Followers: " + str(followers))
-
-followers += 50
-print("Followers: " + str(followers))
-
-followers += 20
-print("Followers: " + str(followers))
-
-followers += 10
-print("Followers: " + str(followers))
+print("Age: " + age)
+print("Category: " + category)
