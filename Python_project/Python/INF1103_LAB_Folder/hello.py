dc9fe5123053dedc056afe7c2c083d0d22712327
Joseph <Josephson080@gmail.com>
2026-09-04 14:56:21 +0800

    my second commit

--- Python_project/Python/INF1103_LAB_Folder/hello.py
+++ Python_project/Python/INF1103_LAB_Folder/hello.py
@@ -2,3 +2,11 @@ print("------------------")
 print("Welcome here ")
 print("my first post")
 print("------------------")
+
+username = "john_doe"
+bio = "I am a software developer"
+followers = 100
+
+print("Username: " + username)
+print("Bio: " + bio)
+print("Followers: " + str(followers))
